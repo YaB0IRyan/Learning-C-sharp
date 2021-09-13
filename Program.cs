0a7595175742ec46390ac6ab6c978e778d65cbd2
Ryan Scott <40533651@live.napier.ac.uk>
2021-09-14 00:17:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Learning_C_sharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, this is an example of text being printed, please press enter to continue"); 
+            //Calls the WriteLine method from console class and writes the contents of the ("") to the console.
+
+            Console.ReadKey();
+            //Calls the ReadKey method from console class and pauses the execution to wait for the user to press an key.
+
+
+        }
+    }
+}
